8fb4fdcf2eb6b5136255303a53a3754ea9c9aaef
suras <msurendar@outlook.com>
2024-07-03 14:04:43 +0530

    Add system tests for login and sign up

---
+++ test/system/mobile/navigation_bar_test.rb
@@ -0,0 +1,24 @@
+require "application_system_test_case"
+
+module Mobile
+    class NavigationBarTest < MobileSystemTestCase
+       test "can access sign up page via burger menu" do
+          visit root_path
+
+          find(".navbar-burger").click
+          click_on I18n.t("application.navbar.sign_up")
+
+          assert_current_path sign_up_path
+       end
+
+       test "can access login page via burger menu" do
+         visit root_path
+ 
+         find(".navbar-burger").click
+         click_on I18n.t("application.navbar.login")
+
+         assert_current_path login_path
+      end
+
+    end
+end
---
+++ test/system/users_test.rb
@@ -0,0 +1,55 @@
+require "application_system_test_case"
+
+class UsersTest < ApplicationSystemTestCase
+  test "new user can sign up" do
+    visit root_path
+
+    click_on I18n.t("application.navbar.sign_up")
+    
+    fill_in User.human_attribute_name(:name), with: "Newman"
+    fill_in User.human_attribute_name(:email), with: "newman@example.com"
+    fill_in User.human_attribute_name(:password), with: "short"
+    fill_in User.human_attribute_name(:password_confirmation), with: "short"
+
+    
+
+    click_on I18n.t("users.new.sign_up")
+    assert_selector "p.is-danger", text: I18n.t("activerecord.errors.models.user.attributes.password.too_short")
+
+    fill_in User.human_attribute_name(:password), with: "password"
+    fill_in User.human_attribute_name(:password_confirmation), with: "password"
+
+    click_on I18n.t("users.new.sign_up")
+
+    assert_current_path root_path
+    assert_selector ".notification", text: I18n.t("users.create.welcome", name: "Newman")
+    assert_selector ".navbar-dropdown", visible: false
+  end  
+  
+  test "existing user can login" do
+    visit root_path
+
+    click_on I18n.t("application.navbar.login")
+
+    fill_in User.human_attribute_name(:email), with: "jerry@emample.com"
+    fill_in User.human_attribute_name(:password), with: "wrong"
+
+    click_button I18n.t("sessions.new.submit")
+
+    assert_selector ".notification.is-danger", text: I18n.t("sessions.create.incorrect_details")
+
+    fill_in User.human_attribute_name(:email), with: "jerry@example.com"
+    fill_in User.human_attribute_name(:password), with: "password"
+
+    click_button I18n.t("sessions.new.submit")
+
+    assert_current_path root_path
+
+    assert_selector ".notification", text: I18n.t("sessions.create.success")
+    assert_selector ".navbar-dropdown", visible: false
+
+  end
+
+
+
+end
