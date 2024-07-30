2c7ab1388c728ed8179c2855c3f9b7551b4a2e0b
suras <msurendar@outlook.com>
2024-07-30 18:02:55 +0530

    added listing validation

--- test/models/listing_test.rb
+++ test/models/listing_test.rb
@@ -1,7 +1,24 @@
 require "test_helper"
 
 class ListingTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+
+  setup do
+    @user = users(:jerry)
+  end
+  test "User should create a listing with valid params" do
+    listing = Listing.new(title: "The world is one", price: 100, creator: @user, organization: @user.organizations.first)
+    assert listing.valid?
+  end
+
+
+  test "User should not be able create a listing with short title count" do
+    listing = Listing.new(title: "The", price: 100, creator: @user, organization: @user.organizations.first)
+    assert_not listing.valid?
+  end
+
+  test "User should not be able create a listing with invalid price" do
+    listing = Listing.new(title: "The", price: "vvv", creator: @user, organization: @user.organizations.first)
+    assert_not listing.valid?
+  end
+
 end
